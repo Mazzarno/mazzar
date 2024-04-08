b8ca35dc4ed811d93c571432958a99fb3aaa90cf
Mazzarno <alexis-germain@live.fr>
2024-04-08 16:52:56 +0200

    0.0.7

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,60 @@
 <template>
-  <div class="container"></div>
+  <div class="scroll_container">
+    <div class="section">
+      <p>Section 1</p>
+    </div>
+    <div class="section">
+      <p>Section 2</p>
+    </div>
+    <div class="section">
+      <p>Section 3</p>
+    </div>
+    <div class="section">
+      <p>Section 4</p>
+    </div>
+    <div class="section">
+      <p>Section 5</p>
+    </div>
+    <div class="section">
+      <p>Section 6</p>
+    </div>
+    <div class="section">
+      <p>Section 7</p>
+    </div>
+    <div class="section">
+      <p>Section 8</p>
+    </div>
+    <div class="section">
+      <p>Section 9</p>
+    </div>
+    <div class="section">
+      <p>Section 10</p>
+    </div>
+    <div class="section">
+      <p>Section 11</p>
+    </div>
+    <div class="section">
+      <p>Section 12</p>
+    </div>
+    <div class="section">
+      <p>Section 13</p>
+    </div>
+    <div class="section">
+      <p>Section 14</p>
+    </div>
+    <div class="section">
+      <p>Section 15</p>
+    </div>
+    <div class="section">
+      <p>Section 16</p>
+    </div>
+    <div class="section">
+      <p>Section 17</p>
+    </div>
+    <div class="section">
+      <p>Section 18</p>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -11,8 +66,36 @@ if (process.client) {
 }
 </script>
 
-<style>
-.container {
-  height: 200vh;
+<style scoped>
+.scroll_container {
+  overflow: auto;
+  scroll-snap-type: both mandatory;
+  width: 35vw;
+  height: 30vh;
+  white-space: nowrap; /* Set inline elemens to not be wrapped, ie, stay on the x-axis */
+}
+
+.section {
+  width: 30vw;
+  height: 30vh;
+  scroll-snap-align: start;
+}
+
+/* Contrive some sections to go left to right */
+
+.section:nth-of-type(4n),
+.section:nth-of-type(4n + 1),
+.section:nth-of-type(4n + 2) {
+  display: inline-block;
+}
+
+/* Make sections stand out.*/
+
+.scroll_container > div:nth-of-type(odd).section {
+  background: pink;
+}
+
+.scroll_container > div:nth-of-type(even).section {
+  background: wheat;
 }
 </style>
--- pages/v.vue
+++ pages/v.vue
@@ -3,37 +3,100 @@ const { onLoop } = useRenderLoop()
 const { $anime } = useNuxtApp()
 const scRef = ref()
 const camRef = ref()
+const lineRef = ref()
 const progress = ref(0)
+const stepone = true
+const steptwo = false
+const stepthree = false
+const stepfour = false
+const stepfive = false
+
+let points = [
+  [0, 0.1, 0],
+  [0, -1, 0],
+]
+
+// INTRO
 onLoop(({ delta }) => {
   if (camRef.value.position.z > 10) {
     camRef.value.position.z -= delta * 100
   }
 })
-const y = 2
-/*
-onMounted(() => {
-  if (useScroll.isScrolling === true) {
+
+onLoop(({ delta }) => {
+  if (lineRef.value) {
+    points = [
+      [0, delta * 100, 0],
+      [0, -delta * 100, 0],
+    ]
+  }
+})
+
+// SCROLL
+onLoop(() => {
+  stepOne()
+  stepTwo()
+  stepThree()
+  stepFour()
+  stepFive()
+})
+function stepOne() {
+  if (progress.value <= 0.15) {
     $anime({
-      targets: boxRef.value.value.position,
-      y: 10,
-      x: 20,
-      easing: 'easeInOutSine',
-      duration: 5000,
-      direction: 'alternate',
-      loop: true,
+      targets: camRef.value.position,
+      y: 2,
+      duration: 1000,
     })
   }
-})*/ function scrollUp() {
+}
+function stepTwo() {
+  if (progress.value >= 0.2 && progress.value <= 0.4) {
+    $anime({
+      targets: camRef.value.position,
+      y: -8,
+      duration: 1000,
+    })
+  }
+}
+function stepThree() {
+  if (progress.value >= 0.4 && progress.value <= 0.6) {
+    $anime({
+      targets: camRef.value.position,
+      y: -18,
+      duration: 1000,
+    })
+  }
+}
+function stepFour() {
+  if (progress.value >= 0.6 && progress.value <= 0.8) {
+    $anime({
+      targets: camRef.value.position,
+      y: -28,
+      duration: 1000,
+    })
+  }
+}
+function stepFive() {
+  if (progress.value >= 0.8 && progress.value <= 1) {
+    $anime({
+      targets: camRef.value.position,
+      y: -38,
+      duration: 1000,
+    })
+  }
+}
+/*
+function scrollUp() {
   $anime({
     targets: camRef.value.position,
-    y: ,
+    y: up,
     duration: 1000,
   })
 }
 function scrollDown() {
   $anime({
     targets: camRef.value.position,
-    y: camRef.value.position * 10,
+    y: down,
     duration: 1000,
   })
 }
@@ -45,12 +108,18 @@ if (process.client) {
       window.addEventListener('wheel', scrollDown)
     }
   })
-}
+})*/
 </script>
 <template>
   <div id="app">
     <TresCanvas clear-color="#000000" id="canvas">
       <TresPerspectiveCamera :position="[0, 2, 200]" :fov="75" ref="camRef" />
+      <ScrollControls
+        v-model="progress"
+        :pages="1"
+        :distance="0"
+        ref="scRef"
+      ></ScrollControls>
       <TresAmbientLight :intensity="1" />
       <Levioso :speed="3" :floatFactor="5" :rotationFactor="0.1">
         <MouseParallax :factor="2" :ease="3" />
@@ -58,7 +127,13 @@ if (process.client) {
 
       <boxi :position="[-5, 1, 0]" :rotation="[0, 0.5, 0]" />
       <boxi :position="[5, 5, 0]" :rotation="[0, 0.3, 0]" />
-
+      <line2
+        ref="lineRef"
+        color="#FBFBFB"
+        :line-width="2"
+        :position="[0, 1.25, 0]"
+        :points="points"
+      />
       <Precipitation
         :randomness="1"
         :speed="0.01"
@@ -69,8 +144,6 @@ if (process.client) {
       <boxi :position="[-15, -1, -10]" :rotation="[0, 0.5, 0]" />
       <boxi :position="[15, -5, -8]" :rotation="[0, 0.3, 0]" />
       <boxi :position="[10, 12, -15]" :rotation="[0, 0.1, 0]" />
-      <!-- BOXI TEST-->
-
       <TresGridHelper :position="[0, 0, 0]" :args="[10, 10]" />
       <TresGridHelper :position="[0, -10, 0]" :args="[10, 10]" />
       <TresGridHelper :position="[0, -20, 0]" :args="[10, 10]" />
@@ -79,9 +152,6 @@ if (process.client) {
   </div>
 </template>
 <style scoped>
-#canvas {
-  overflow-y: scroll;
-}
 #app {
   height: 100vh;
   width: 100vw;
