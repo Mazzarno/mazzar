50aa796314d9e0b0c58847226eb12d11b298b5ff
Alexis Germain <alex@macbook-pro-de-alexis.home>
2024-04-08 18:43:13 +0200

    0.0.7

--- pages/v.vue
+++ pages/v.vue
@@ -1,89 +1,91 @@
 <script setup>
-const { onLoop } = useRenderLoop()
-const { $anime } = useNuxtApp()
-const scRef = ref()
-const camRef = ref()
-const lineRef = ref()
-const progress = ref(0)
-const stepone = true
-const steptwo = false
-const stepthree = false
-const stepfour = false
-const stepfive = false
+const { onLoop } = useRenderLoop();
+const { $anime } = useNuxtApp();
+const scRef = ref();
+const camRef = ref();
+const lineRef = ref();
+const progress = ref(0);
+
+const oneStep = ref(true);
+const twoStep = ref(false);
+const threeStep = ref(false);
+const fourStep = ref(false);
+const fiveStep = ref(false);
 
 let points = [
   [0, 0.1, 0],
   [0, -1, 0],
-]
+];
 
 // INTRO
 onLoop(({ delta }) => {
   if (camRef.value.position.z > 10) {
-    camRef.value.position.z -= delta * 100
+    camRef.value.position.z -= delta * 100;
   }
-})
-
-onLoop(({ delta }) => {
-  if (lineRef.value) {
-    points = [
-      [0, delta * 100, 0],
-      [0, -delta * 100, 0],
-    ]
-  }
-})
+});
 
 // SCROLL
 onLoop(() => {
-  stepOne()
-  stepTwo()
-  stepThree()
-  stepFour()
-  stepFive()
-})
-function stepOne() {
-  if (progress.value <= 0.15) {
-    $anime({
-      targets: camRef.value.position,
-      y: 2,
-      duration: 1000,
-    })
+  if (progress.value <= 0.05) {
+    stepOne();
   }
+  if (progress.value >= 0.1 && progress.value <= 0.4) {
+    stepTwo();
+  }
+  if (progress.value >= 0.4 && progress.value <= 0.6) {
+    stepThree();
+  }
+  if (progress.value >= 0.6 && progress.value <= 0.8) {
+    stepFour();
+  }
+  if (progress.value >= 0.8 && progress.value <= 1) {
+    stepFive();
+  }
+});
+function stepOne() {
+  progress.value = 0;
+  oneStep.value = true;
+  $anime({
+    targets: camRef.value.position,
+    y: 2,
+    duration: 1000,
+  });
 }
 function stepTwo() {
-  if (progress.value >= 0.2 && progress.value <= 0.4) {
-    $anime({
-      targets: camRef.value.position,
-      y: -8,
-      duration: 1000,
-    })
-  }
+  progress.value = 0.2;
+  twoStep.value = true;
+  $anime({
+    targets: camRef.value.position,
+    y: -8,
+    duration: 1000,
+  });
 }
 function stepThree() {
-  if (progress.value >= 0.4 && progress.value <= 0.6) {
-    $anime({
-      targets: camRef.value.position,
-      y: -18,
-      duration: 1000,
-    })
-  }
+  progress.value = 0.5;
+  threeStep.value = true;
+  $anime({
+    targets: camRef.value.position,
+    y: -18,
+    duration: 1000,
+  });
 }
 function stepFour() {
-  if (progress.value >= 0.6 && progress.value <= 0.8) {
-    $anime({
-      targets: camRef.value.position,
-      y: -28,
-      duration: 1000,
-    })
-  }
+  progress.value = 0.7;
+  fourStep.value = true;
+  $anime({
+    targets: camRef.value.position,
+    y: -28,
+    duration: 1000,
+  });
 }
 function stepFive() {
-  if (progress.value >= 0.8 && progress.value <= 1) {
-    $anime({
-      targets: camRef.value.position,
-      y: -38,
-      duration: 1000,
-    })
-  }
+  progress.value = 0.9;
+  fiveStep.value = true;
+  $anime({
+    targets: camRef.value.position,
+    y: -38,
+    duration: 1000,
+  });
 }
 /*
 function scrollUp() {
@@ -109,9 +111,38 @@ if (process.client) {
     }
   })
 })*/
+console.log(oneStep);
+console.log(twoStep);
+console.log(threeStep);
+console.log(fourStep);
+console.log(fiveStep);
 </script>
 <template>
   <div id="app">
+    <div
+      class="absolute top-1/2 left-full transform -translate-x-full -translate-y-1/2 z-50 items-center"
+    >
+      <div
+        @click="stepOne"
+        class="h-4 w-4 bg-slate-500 opacity-50 rounded-full m-5 hover:opacity-90 active:opacity-100 active"
+      ></div>
+      <div
+        @click="stepTwo"
+        class="h-4 w-4 bg-slate-500 opacity-50 rounded-full m-5 hover:opacity-90 active:opacity-100"
+      ></div>
+      <div
+        @click="stepThree"
+        class="h-4 w-4 bg-slate-500 opacity-50 rounded-full m-5 hover:opacity-90 active:opacity-100"
+      ></div>
+      <div
+        @click="stepFour"
+        class="h-4 w-4 bg-slate-500 opacity-50 rounded-full m-5 hover:opacity-90 active:opacity-100"
+      ></div>
+      <div
+        @click="stepFive"
+        class="h-4 w-4 bg-slate-500 opacity-50 rounded-full m-5 hover:opacity-90 active:opacity-100"
+      ></div>
+    </div>
     <TresCanvas clear-color="#000000" id="canvas">
       <TresPerspectiveCamera :position="[0, 2, 200]" :fov="75" ref="camRef" />
       <ScrollControls
@@ -155,5 +186,10 @@ if (process.client) {
 #app {
   height: 100vh;
   width: 100vw;
+  -ms-overflow-style: none; /* Internet Explorer 10+ */
+  scrollbar-width: none; /* Firefox */
+}
+#app::-webkit-scrollbar {
+  display: none; /* Safari and Chrome */
 }
 </style>
