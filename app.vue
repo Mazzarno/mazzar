f68926f206cf629d6750b2e6b47b48c8e2b87314
Mazzarno <alexis-germain@live.fr>
2024-04-13 15:08:38 +0200

    0.0.9

--- app.vue
+++ app.vue
@@ -1,117 +1,3 @@
-<script setup>
-const { onLoop } = useRenderLoop();
-const { $anime } = useNuxtApp();
-const scRef = ref();
-const camRef = ref();
-const lineRef = ref();
-const progress = ref(0);
-
-const oneStep = ref(true);
-const twoStep = ref(false);
-const threeStep = ref(false);
-const fourStep = ref(false);
-const fiveStep = ref(false);
-
-let points = [
-  [0, 0.1, 0],
-  [0, -1, 0],
-];
-
-// INTRO
-onLoop(({ delta }) => {
-  if (camRef.value.position.z > 10) {
-    camRef.value.position.z -= delta * 100;
-  }
-});
-
-// SCROLL
-onLoop(() => {
-  if (progress.value <= 0.05) {
-    stepOne();
-  }
-  if (progress.value >= 0.1 && progress.value <= 0.4) {
-    stepTwo();
-  }
-  if (progress.value >= 0.4 && progress.value <= 0.6) {
-    stepThree();
-  }
-  if (progress.value >= 0.6 && progress.value <= 0.8) {
-    stepFour();
-  }
-  if (progress.value >= 0.8 && progress.value <= 1) {
-    stepFive();
-  }
-});
-function stepOne() {
-  progress.value = 0;
-  oneStep.value = true;
-  $anime({
-    targets: camRef.value.position,
-    y: 2,
-    duration: 1000,
-  });
-}
-function stepTwo() {
-  progress.value = 0.2;
-  twoStep.value = true;
-  $anime({
-    targets: camRef.value.position,
-    y: -8,
-    duration: 1000,
-  });
-}
-function stepThree() {
-  progress.value = 0.5;
-  threeStep.value = true;
-  $anime({
-    targets: camRef.value.position,
-    y: -18,
-    duration: 1000,
-  });
-}
-function stepFour() {
-  progress.value = 0.7;
-  fourStep.value = true;
-  $anime({
-    targets: camRef.value.position,
-    y: -28,
-    duration: 1000,
-  });
-}
-function stepFive() {
-  progress.value = 0.9;
-  fiveStep.value = true;
-  $anime({
-    targets: camRef.value.position,
-    y: -38,
-    duration: 1000,
-  });
-}
-/*
-function scrollUp() {
-  $anime({
-    targets: camRef.value.position,
-    y: up,
-    duration: 1000,
-  })
-}
-function scrollDown() {
-  $anime({
-    targets: camRef.value.position,
-    y: down,
-    duration: 1000,
-  })
-}
-if (process.client) {
-  window.addEventListener('wheel', function (event) {
-    if (event.deltaY < 0) {
-      window.addEventListener('wheel', scrollUp)
-    } else if (event.deltaY > 0) {
-      window.addEventListener('wheel', scrollDown)
-    }
-  })
-})*/
-</script>
 <template>
   <div id="app">
     <div
@@ -119,47 +5,38 @@ if (process.client) {
     >
       <div
         @click="stepOne"
-        class="h-4 w-4 bg-slate-500 opacity-50 rounded-full m-5 hover:opacity-90 active:opacity-100 active"
+        :class="{ 'animate-pulse bg-white': oneStep }"
+        class="h-4 w-4 bg-slate-600 bg-opacity-90 rounded-full m-5 hover:bg-opacity-100"
       ></div>
       <div
         @click="stepTwo"
-        class="h-4 w-4 bg-slate-500 opacity-50 rounded-full m-5 hover:opacity-90 active:opacity-100"
+        :class="{ 'animate-pulse bg-white': twoStep }"
+        class="h-4 w-4 bg-slate-600 bg-opacity-90 rounded-full m-5 hover:bg-opacity-100"
       ></div>
       <div
         @click="stepThree"
-        class="h-4 w-4 bg-slate-500 opacity-50 rounded-full m-5 hover:opacity-90 active:opacity-100"
+        :class="{ 'animate-pulse bg-white': threeStep }"
+        class="h-4 w-4 bg-slate-600 bg-opacity-90 rounded-full m-5 hover:bg-opacity-100"
       ></div>
       <div
         @click="stepFour"
-        class="h-4 w-4 bg-slate-500 opacity-50 rounded-full m-5 hover:opacity-90 active:opacity-100"
+        :class="{ 'animate-pulse bg-white': fourStep }"
+        class="h-4 w-4 bg-slate-600 bg-opacity-90 rounded-full m-5 hover:bg-opacity-100"
       ></div>
       <div
         @click="stepFive"
-        class="h-4 w-4 bg-slate-500 opacity-50 rounded-full m-5 hover:opacity-90 active:opacity-100"
+        :class="{ 'animate-pulse bg-white': fiveStep }"
+        class="h-4 w-4 bg-slate-600 bg-opacity-90 rounded-full m-5 hover:bg-opacity-100"
       ></div>
     </div>
     <TresCanvas clear-color="#000000" id="canvas">
-      <TresPerspectiveCamera :position="[0, 2, 200]" :fov="75" ref="camRef" />
-      <ScrollControls
-        v-model="progress"
-        :pages="1"
-        :distance="0"
-        ref="scRef"
-      ></ScrollControls>
+      <TresPerspectiveCamera :position="[0, 2, 50]" :fov="75" ref="camRef" />
       <TresAmbientLight :intensity="1" />
       <Levioso :speed="3" :floatFactor="5" :rotationFactor="0.1">
         <MouseParallax :factor="2" :ease="3" />
       </Levioso>
-
       <boxi :position="[-5, 1, 0]" :rotation="[0, 0.5, 0]" />
       <boxi :position="[5, 5, 0]" :rotation="[0, 0.3, 0]" />
-      <line2
-        ref="lineRef"
-        color="#FBFBFB"
-        :line-width="2"
-        :position="[0, 1.25, 0]"
-        :points="points"
-      />
       <Precipitation
         :randomness="1"
         :speed="0.01"
@@ -167,22 +44,191 @@ if (process.client) {
         :area="[75, 50, 100]"
       />
       <stars />
+      <Suspense>
+        <Smoke ref="smokeRef" texture="./public/assets/sprite-smoke.png" />
+      </Suspense>
       <boxi :position="[-15, -1, -10]" :rotation="[0, 0.5, 0]" />
       <boxi :position="[15, -5, -8]" :rotation="[0, 0.3, 0]" />
       <boxi :position="[10, 12, -15]" :rotation="[0, 0.1, 0]" />
       <TresGridHelper :position="[0, 0, 0]" :args="[10, 10]" />
-      <TresGridHelper :position="[0, -10, 0]" :args="[10, 10]" />
       <TresGridHelper :position="[0, -20, 0]" :args="[10, 10]" />
-      <TresGridHelper :position="[0, -30, 0]" :args="[10, 10]" />
+      <TresGridHelper :position="[0, -40, 0]" :args="[10, 10]" />
+      <TresGridHelper :position="[0, -60, 0]" :args="[10, 10]" />
+      <TresGridHelper :position="[0, -80, 0]" :args="[10, 10]" />
     </TresCanvas>
   </div>
 </template>
+<script setup>
+const { onLoop } = useRenderLoop()
+const { $anime } = useNuxtApp()
+const camRef = ref()
+const smokeRef = ref()
+const oneStep = ref(true)
+const twoStep = ref(false)
+const threeStep = ref(false)
+const fourStep = ref(false)
+const fiveStep = ref(false)
+console.log(smokeRef)
+const steps = [
+  { id: 0, name: 'one', value: { ycam: 2, snap: [10, -10], func: stepOne } },
+  {
+    id: 1,
+    name: 'two',
+    value: { ycam: -18, snap: [-10, -30], func: stepTwo, active: true },
+  },
+  {
+    id: 2,
+    name: 'three',
+    value: { ycam: -38, snap: [-30, -50], func: stepThree, active: false },
+  },
+  {
+    id: 3,
+    name: 'four',
+    value: { ycam: -58, snap: [-50, -70], func: stepFour, active: false },
+  },
+  {
+    id: 4,
+    name: 'five',
+    value: { ycam: -78, snap: [-70, -90], func: stepFive, active: false },
+  },
+]
+let currentIndex = 0
+let isFunctionExecuted = false // Variable pour suivre si une fonction a été utilisée
+let timer // Variable pour stocker le temporisateur
+
+const handleScroll = (event) => {
+  if (isFunctionExecuted) {
+    // Si une fonction a été utilisée récemment, ignore le scroll
+    return
+  }
+
+  // Récupère la variation du défilement de la molette de la souris sur l'axe Y
+  const deltaY = event.deltaY
+
+  // Détermine le sens du défilement en fonction de la variation
+  let scrollDirection
+  if (deltaY > 0) {
+    scrollDirection = 'down'
+  } else if (deltaY < 0) {
+    scrollDirection = 'up'
+  }
+
+  // Met à jour l'index en fonction du sens du défilement
+  if (scrollDirection === 'down') {
+    if (currentIndex < steps.length - 1) {
+      currentIndex++ // Incrémente l'index si ce n'est pas la limite supérieure
+    }
+  } else if (scrollDirection === 'up') {
+    if (currentIndex > 0) {
+      currentIndex-- // Décrémente l'index si ce n'est pas la limite inférieure
+    }
+  }
+
+  // Applique la valeur ycam correspondante
+  const newYcam = steps[currentIndex].value.ycam
+
+  // Exécute la fonction func correspondante
+  const funcToExecute = steps[currentIndex].value.func
+  funcToExecute()
+  // Empêche l'utilisation du scroll pendant 1 secondes
+  isFunctionExecuted = true
+  timer = setTimeout(() => {
+    isFunctionExecuted = false
+  }, 1000)
+}
+
+onMounted(() => {
+  // Ajoute un écouteur d'événements pour détecter le défilement
+  window.addEventListener('wheel', handleScroll)
+})
+
+onUnmounted(() => {
+  // Retire l'écouteur d'événements lors de la destruction du composant
+  window.removeEventListener('wheel', handleScroll)
+})
+
+onLoop(({ delta }) => {
+  if (camRef.value.position.z > 10) {
+    camRef.value.position.z -= delta * 100
+  }
+})
+function stepOne() {
+  oneStep.value = true
+  twoStep.value = false
+  threeStep.value = false
+  fourStep.value = false
+  fiveStep.value = false
+
+  $anime({
+    targets: camRef.value.position,
+    y: 2,
+    duration: 1000,
+  })
+}
+function stepTwo() {
+  oneStep.value = false
+  twoStep.value = true
+  threeStep.value = false
+  fourStep.value = false
+  fiveStep.value = false
+
+  $anime({
+    targets: camRef.value.position,
+    y: -18,
+    duration: 1000,
+  })
+}
+function stepThree() {
+  oneStep.value = false
+  twoStep.value = false
+  threeStep.value = true
+  fourStep.value = false
+  fiveStep.value = false
+
+  $anime({
+    targets: camRef.value.position,
+    y: -38,
+    duration: 1000,
+  })
+}
+function stepFour() {
+  oneStep.value = false
+  twoStep.value = false
+  threeStep.value = false
+  fourStep.value = true
+  fiveStep.value = false
+
+  $anime({
+    targets: camRef.value.position,
+    y: -58,
+    duration: 1000,
+  })
+}
+function stepFive() {
+  oneStep.value = false
+  twoStep.value = false
+  threeStep.value = false
+  fourStep.value = false
+  fiveStep.value = true
+
+  $anime({
+    targets: camRef.value.position,
+    y: -78,
+    duration: 1000,
+  })
+}
+</script>
+
 <style scoped>
 #app {
   height: 100vh;
   width: 100vw;
   -ms-overflow-style: none; /* Internet Explorer 10+ */
   scrollbar-width: none; /* Firefox */
+}
+#canvas {
+  height: 100vh;
+  width: 100vw;
 }
 #app::-webkit-scrollbar {
   display: none; /* Safari and Chrome */
